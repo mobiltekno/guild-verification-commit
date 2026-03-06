74c5bdde052185debfee9ee2a8c63d4e6da2563b
mobiltekno <qundi77@gmail.com>
2026-03-06 03:28:44 +0300

    Fix broken links in documentation

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-06T00:28:34.995Z
-Commit: Add export functionality for reports
+Timestamp: 2026-03-06T00:28:45.437Z
+Commit: Fix broken links in documentation
