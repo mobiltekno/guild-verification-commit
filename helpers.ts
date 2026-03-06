9c0736c9f4838938d8a30fa5cf93ff3193e20cbe
mobiltekno <qundi77@gmail.com>
2026-03-06 03:28:48 +0300

    Add export functionality for reports

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-06T00:28:45.437Z
-Commit: Fix broken links in documentation
+Timestamp: 2026-03-06T00:28:49.015Z
+Commit: Add export functionality for reports
