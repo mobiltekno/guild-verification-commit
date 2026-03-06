bc89da1f71612926d56d4fad2af5d7ba20ced0d0
mobiltekno <qundi77@gmail.com>
2026-03-06 03:29:02 +0300

    Fix memory leak in event handlers

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-06T00:29:00.497Z
-Commit: Fix CSS styling bug on mobile devices
+Timestamp: 2026-03-06T00:29:03.206Z
+Commit: Fix memory leak in event handlers
