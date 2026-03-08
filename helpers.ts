16e0a787f167192393ca8bc6de315e7c3cb53f5e
mobiltekno <qundi77@gmail.com>
2026-03-09 02:02:44 +0300

    Fix memory leak in event handlers

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-06T00:29:46.332Z
-Commit: Introduce real-time notifications
+Timestamp: 2026-03-08T23:02:43.329Z
+Commit: Fix memory leak in event handlers
